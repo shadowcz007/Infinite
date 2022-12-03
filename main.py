2a4582f7717c2cd78f8da368e912fcbc5e16a279
shadow <chizhiwei007@163.com>
2022-12-03 22:42:11 +0800

    Add files via upload

--- main.py
+++ main.py
@@ -1,19 +1,15 @@
 import random
 import hashlib
 import time
-import json
+import json,os
 
 from PIL import ImageGrab,Image
 import gradio as gr
 
 
-# huggingface的sd库
-from diffusers import StableDiffusionPipeline,StableDiffusionImg2ImgPipeline
-import torch
-
 DEVICE="cuda"
-MODEL_ID = "IDEA-CCNL/Taiyi-Stable-Diffusion-1B-Chinese-EN-v0.1"
-
+# MODEL_ID = "IDEA-CCNL/Taiyi-Stable-Diffusion-1B-Chinese-EN-v0.1"
+MODEL_ID="IDEA-CCNL/Taiyi-Stable-Diffusion-1B-Chinese-v0.1"
 
 # 内置的物体名词
 KEYWORDS=[k.strip() for k in 
@@ -35,11 +31,14 @@ STYLE_PROMPTS=[k.strip() for k in
 
 SCREE_PATH='jieping.png'
 JSON_PATH='sd/images.json'
-USER_COUNT_PATH='sd/user.json'
+ 
 
 ocr=None
 pipe_text2img=None
 pipe_img2img=None
+
+# httpserver
+http_server=None
 
 #上一次用户输入
 pre_text='1'
@@ -58,6 +57,10 @@ def init_ocr():
     ocr = ppppocr.ppppOcr(model='server')
 
 def init_sd():
+    # huggingface的sd库
+    from diffusers import StableDiffusionPipeline,StableDiffusionImg2ImgPipeline
+    import torch
+
     global pipe_text2img
     global pipe_img2img
     
@@ -66,7 +69,7 @@ def init_sd():
     # global sd_zh_pipe
     # sd_zh_pipe = StableDiffusionPipeline.from_pretrained(model_id, torch_dtype=torch.float16).to(device)
     # sd_zh_pipe = StableDiffusionPipeline.from_pretrained("IDEA-CCNL/Taiyi-Stable-Diffusion-1B-Chinese-EN-v0.1").to("cuda")
-    pipe_text2img = StableDiffusionPipeline.from_pretrained(MODEL_ID).to(DEVICE)
+    pipe_text2img = StableDiffusionPipeline.from_pretrained(MODEL_ID, torch_dtype=torch.float16,safety_checker=None).to(DEVICE)
     pipe_img2img = StableDiffusionImg2ImgPipeline(**pipe_text2img.components).to(DEVICE)
 
  
@@ -77,7 +80,6 @@ def random_prompt_style():
 def random_keyword():
     i=random.randint(0,len(KEYWORDS)-1)
     return KEYWORDS[i]
-
 
 def load_json():
     f = open(JSON_PATH, 'r')
@@ -97,6 +99,17 @@ def write_json(a,file_path):
     return
 
 
+def create_http_server():
+    global http_server
+    if http_server==None:
+        http_server=os.popen('python3 -m http.server')
+        os.popen('start http://localhost:8000/magic-card.html')
+
+def stop_http_server():
+    global http_server
+    if http_server:
+        http_server.close()
+
 def shotscreen():
     global screen_area
     img = ImageGrab.grab(bbox=(screen_area[0],screen_area[1], screen_area[2],screen_area[3])) #四个数字分别是要截屏的四个角
@@ -115,18 +128,6 @@ def md5(t):
     return hashlib.md5(t.encode(encoding='UTF-8')).hexdigest()
 
 
-def create_image(text):
-    global pipe_text2img
-    if pipe_text2img==None:
-        init_sd()
-
-    images=pipe_text2img(text+random_prompt_style(), guidance_scale=10.0,num_images_per_prompt=1).images
-    name='result_'+text+'_'+str(int(time.time()))
-    name=md5(name)+'.png'
-    images[0].save('sd/'+name)
-    return name,images[0]
-
-
 def readtext():
     global ocr
     if ocr==None:
@@ -142,76 +143,48 @@ def get_user_input():
     result = readtext()
 
     res=[]
-    for t in result:
-        text=t[0]
-        if text!='主播':
-            text=text.replace(':','：')
-            texts=text.split('：')
-            if len(texts)==1:
-                if(len(res)>0):
-                    res[-1]+=','+texts[0]
-            elif len(texts)==2:
-                if texts[1]=='':
-                    res.append(1)
-                else:
-                    num=texts[1]
-                    try:
-                        num=int(num)
-                    except:
-                        num=None
-                    if num!=None:
-                        res.append(num)
-                    else:
-                        res.append(texts[1])       
+    if result!=None:
+        for t in result:
+            if t!=None:
+                text=t[0]
+                if text!='主播':
+                    text=text.replace(':','：')
+                    texts=text.split('：')
+                    if len(texts)==1:
+                        if(len(res)>0):
+                            res[-1]+=','+texts[0]
+                    elif len(texts)==2:
+                        if texts[1]=='':
+                            res.append(1)
+                        else:
+                            num=texts[1]
+                            try:
+                                num=int(num)
+                            except:
+                                num=None
+                            if num!=None:
+                                res.append(num)
+                            else:
+                                res.append(texts[1])       
     return res,im 
 
 
-#根据用户输入，新主题生成图片
-def write_result():
+
+
+#根据用户输入，新主题 
+def get_user_input_and_prompt():
     global pre_text
     #用户输入
     texts,im=get_user_input()
 
-    #记录用户的投票
-    #write_user_feedback(texts)
-    
     # 避免 1 作为prompt
     # print('==========',texts)
     if(len(texts)>0 and texts[-1].strip()!="" and texts[-1].strip()!="1" and texts[-1]!=pre_text):
-        input_text=texts[-1].strip()
-        tid=md5(input_text)
-        result_images={
-            "id":tid,
-            "input":input_text,
-            "images":[]
-        }
+        pre_text=texts[-1].strip()
+      
+    return im,pre_text
+   
 
-        # 第一张图
-        img_file_name,im=create_image(input_text)
-        result_images['images'].append(img_file_name)
-        save_json(result_images)
-        pre_text=input_text
-
-        return im,result_images,pre_text
-    else:
-        return create_more()
-
-#按照上一个主题生成更多
-def create_more():
-    global pre_text
-    if pre_text==None:
-        return
-    tid=md5(pre_text)
-    result_images={
-            "id":tid,
-            "input":pre_text,
-            "images":[]
-        }
-    # 第n张图
-    img_file_name,im=create_image(pre_text)
-    result_images['images'].append(img_file_name)
-    save_json(result_images)
-    return im,result_images,pre_text
 
 # 获取用户投票
 def count_user_feedback(keyword):
@@ -227,9 +200,13 @@ def count_user_feedback(keyword):
     return count
 
 
-def infer_text2img(prompt, guide, steps, width, height, image_in, strength):
+def infer_text2img(prompt, style_prompt,guide, steps, width, height, image_in, strength):
     global pipe_text2img
     global pipe_img2img
+    if style_prompt!=None:
+        prompt=prompt+','+style_prompt
+    if pipe_text2img==None:
+        init_sd()
     if image_in is not None:
         init_image = image_in.convert("RGB").resize((width, height))
         output = pipe_img2img(prompt, 
@@ -255,30 +232,27 @@ def infer_inpaint(prompt, guide, steps, width, height, image_in):
     image = output.images[0]
     return image
 
-# while True:
-#     try:
-#         write_result()
-#     except:
-#         print('error')
-#     time.sleep(2)
 
 # init_ocr()
 # init_sd()
 
 
 with gr.Blocks(css="main.css") as demo:
-    examples = [[x] for x in STYLE_PROMPTS]
+    examples = [[random_keyword(),x] for x in STYLE_PROMPTS]
     # 截图
     with gr.Row(scale=1,):
         with gr.Column(scale=1, ):
             with gr.Row(scale=0.5 ):
+                
                 # screen_area = gr.Textbox(label = '截图区域',value="620,380, 900,960")
                 screen_x=gr.Slider(0, 1500, value = 620, label = 'x')
                 screen_y=gr.Slider(0, 1500, value = 380, label = 'y')
                 screen_width=gr.Slider(1, 1024, value = 900, label = 'width')
                 screen_height=gr.Slider(1, 1024, value = 960, label = 'height')
+            with gr.Row(scale=0.5 ):
                 shotscreen_btn=gr.Button("截图区域配置")
                 shotscreen_and_ocr_btn=gr.Button("截图&OCR")
+                http_server_btn=gr.Button("http服务")
             
             with gr.Row(scale=0.5 ):
                 guide = gr.Slider(2, 15, value = 7, label = '文本引导强度(guidance scale)')
@@ -292,10 +266,11 @@ with gr.Blocks(css="main.css") as demo:
         with gr.Column(scale=1, ):
             
             keyword = gr.Textbox(label = 'prompt-用户输入')
+
+            style_input = gr.Textbox(label = '风格-预设模板')
             
-            get_user_input_and_createImage_btn=gr.Button("截屏输入生成")
-            create_more_btn=gr.Button("上一次截屏输入生成更多")
-            
+            get_user_input_and_prompt_btn=gr.Button("截屏提取最新回复作为输入")
+         
             submit_btn = gr.Button("根据prompt生成图像")
             shotscreen_and_ocr_and_match_keyword_btn=gr.Button("截屏计算投票")
             
@@ -305,14 +280,14 @@ with gr.Blocks(css="main.css") as demo:
         json_out=gr.JSON(label='结果')    
             
     with gr.Row():     
-        ex = gr.Examples(examples, fn=infer_text2img, inputs=[keyword, guide, steps, width, height], outputs=image_out)
+        ex = gr.Examples(examples, fn=infer_text2img, inputs=[keyword,style_input, guide, steps, width, height], outputs=image_out)
         # with gr.Column(scale=1, ):
         #     image_in = gr.Image(source='upload', tool='sketch', elem_id="image_upload", type="pil", label="Upload")
         #     inpaint_prompt = gr.Textbox(label = '提示词(prompt)')
         #     inpaint_btn = gr.Button("图像编辑(Inpaint)")
             # img2img_prompt = gr.Textbox(label = '提示词(prompt)')
             # img2img_btn = gr.Button("图像编辑(Inpaint)")
-        submit_btn.click(fn = infer_text2img, inputs = [keyword, guide, steps, width, height, image_in, strength], outputs = image_out)
+        submit_btn.click(fn = infer_text2img, inputs = [keyword,style_input, guide, steps, width, height, image_in, strength], outputs = image_out)
 
         shotscreen_btn.click(fn=shotscreen_setup,inputs =[screen_x,screen_y,screen_width,screen_height],
         outputs=image_out,
@@ -322,13 +297,11 @@ with gr.Blocks(css="main.css") as demo:
         outputs=[json_out,image_out],
         api_name="shotscreen")
 
-        get_user_input_and_createImage_btn.click(fn=write_result,
-        outputs=[image_out,json_out,keyword],
-        api_name="get_user_input_and_createImage")
+        http_server_btn.click(fn=create_http_server)
 
-        create_more_btn.click(fn=create_more,
-        outputs=[image_out,json_out,keyword],
-        api_name="create_more")
+        get_user_input_and_prompt_btn.click(fn=get_user_input_and_prompt,
+        outputs=[image_out,keyword],
+        api_name="get_user_input_and_createImage")
 
         shotscreen_and_ocr_and_match_keyword_btn.click(fn=count_user_feedback,
         inputs =[keyword],
